14998f30761dad8819760c0a32dd9a68d7028400
Gilberth Chaves A <gchavesav@gmail.com>
2023-03-29 17:41:17 -0600

    I.S.2023.APA.IF-3001.021-Lab3

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,14 +1,61 @@
-package ucr.lab3;
+package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
+import ucr.lab3.HelloApplication;
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private BorderPane bp;
+    @FXML
+    private AnchorPane ap;
+    @FXML
+    private Text txtMessage;
+
+    private void loadPage(String page){
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(page));
+        try {
+            this.bp.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void Exit(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void Home(ActionEvent event) {
+        this.txtMessage.setText("Laboratory No. 3");
+        this.bp.setCenter(ap);
+    }
+
+    @FXML
+    void divideAndConquerOnAction(ActionEvent event) {
+        loadPage("divideAndConquer.fxml");
+    }
+
+    @FXML
+    void dynamicOnAction(ActionEvent event) {
+        loadPage("dynamic.fxml");
+    }
+
+    @FXML
+    void probabilisticOnAction(ActionEvent event) {
+        loadPage("probabilistic.fxml");
+    }
+
+    @FXML
+    void vectorOnAction(ActionEvent event) {
+        loadPage("vector.fxml");
     }
 }
