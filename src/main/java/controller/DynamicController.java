cfde2eefd703116cebd49f92c9a8f6e6abbd5dc3
Edwar <edward.calvoretana@ucr.ac.cr>
2023-03-31 15:29:25 -0600

    Fibonacci y Factorial

--- src/main/java/controller/DynamicController.java
+++ src/main/java/controller/DynamicController.java
@@ -1,8 +1,9 @@
 package controller;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import domain.Dynamic;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class DynamicController
 {
@@ -12,12 +13,46 @@ public class DynamicController
     private TextField NumberNTF;
     @javafx.fxml.FXML
     private ChoiceBox choiceBox;
-    @javafx.fxml.FXML
-    private Button ClearButton;
-    @javafx.fxml.FXML
-    private Button CalculateButton;
+
+    Alert alert;
 
     @javafx.fxml.FXML
     public void initialize() {
+        alert= alert("Dynamic Option", "There was an error");
+        alert.setAlertType(Alert.AlertType.ERROR);
+        choiceBox.getItems().add("Factorial");
+        choiceBox.getItems().add("Fibonacci");
+    }
+    private boolean isValid() {
+        return !this.NumberNTF.getText().isEmpty();
+    }
+    private Alert alert(String title, String headerText){
+        Alert a= new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle(title);
+        a.setHeaderText(headerText);
+        DialogPane dp= a.getDialogPane();
+        return a;
+    }
 
-    }}
+    public void CalculateButtonOnAction(ActionEvent event) {
+        Dynamic dm = new Dynamic();
+        String value = (String) choiceBox.getValue();
+        int x=0;
+        if(isValid() && value=="Fibonacci"){
+            x=Integer.valueOf(this.NumberNTF.getText());
+            ResultTF.setText(String.valueOf(dm.fibonacci(x)));
+        } else if (isValid() && value=="Factorial") {
+            x=Integer.valueOf(this.NumberNTF.getText());
+            ResultTF.setText(String.valueOf(dm.factorial(x)));
+        }
+
+
+    }
+
+    public void ClearButtonOnAction(ActionEvent actionEvent) {
+        ResultTF.setText("");
+        NumberNTF.setText("");
+    }
+
+
+}
